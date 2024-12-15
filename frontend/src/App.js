31c0c85a729af743db56a431c7e7955ea170a09d
smathur93 <shreymathur93@gmail.com>
2024-12-15 20:50:49 +0530

    Just a minor update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import AddUser from "./Components/experiments/materialUIuserlist/AddUser";
 import ViewUsers from "./Components/experiments/materialUIuserlist/ViewUsers";
 import SearchUsers from "./Components/experiments/materialUIuserlist/SearchUsers";
@@ -9,20 +9,22 @@ import EmployeeList from "./Components/experiments/boot/EmployeeList"
 import UserList from "./Components/UserList";
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route exact path="/" component={UserList} />
-        <Route exact path="/addUser" component={AddUser} />
-        <Route exact path="/viewUsers" component={ViewUsers} />
-        <Route exact path="/searchUsers" component={SearchUsers} />
-        <Route exact path="/productList" component={ProductList} />
-        <Route exact path="/testPage" component={TestPage} />
-        <Route exact path="/employeeList" component={EmployeeList} />
-        <Route exact path="/userList" component={UserList} />
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route exact path="/" component={ProductList} />
+                    <Route exact path="/addUser" component={AddUser} />
+                    <Route exact path="/viewUsers" component={ViewUsers} />
+                    <Route exact path="/searchUsers" component={SearchUsers} />
+                    <Route exact path="/productList" component={ProductList} />
+                    <Route exact path="/testPage" component={TestPage} />
+                    <Route exact path="/employeeList" component={EmployeeList} />
+                    <Route exact path="/userList" component={UserList} />
+                </Routes>
+            </Router>
+        );
+    }
 }
 
 export default App;
